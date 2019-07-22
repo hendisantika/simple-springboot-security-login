7b58b4794a2f6722ca9e4edb3543b70919881def
hendisantika <hendisantika@yahoo.co.id>
2019-07-22 07:47:09 +0700

    #4 - Add User Model

---
+++ src/main/java/com/hendisantika/simplespringbootsecuritylogin/model/User.java
@@ -0,0 +1,53 @@
+package com.hendisantika.simplespringbootsecuritylogin.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.GenerationType.AUTO;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-springboot-security-login
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-22
+ * Time: 07:42
+ */
+@Entity
+@Data
+class User {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = true)
+    private String firstname;
+
+    @Column(nullable = true)
+    private String lastname;
+
+    @Column(nullable = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(STRING)
+    private Role role;
+
+    @Column(nullable = false)
+    private Boolean enabled = false;
+
+    @Column(nullable = false)
+    private Date dateCreate = new Date();
+
+}
