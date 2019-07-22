af334ba95725cbe84fbaf489c946279c41cce639
hendisantika <hendisantika@yahoo.co.id>
2019-07-22 07:39:36 +0700

    #1 - Initial project

---
+++ src/main/java/com/hendisantika/simplespringbootsecuritylogin/SimpleSpringbootSecurityLoginApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.simplespringbootsecuritylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootSecurityLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringbootSecurityLoginApplication.class, args);
+    }
+
+}
