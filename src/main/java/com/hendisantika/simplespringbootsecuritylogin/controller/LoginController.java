843019eee0c4e0939c6c503c3fc8e7e24afe8569
hendisantika <hendisantika@yahoo.co.id>
2019-07-23 05:09:32 +0700

    #10 - Add LoginController

---
+++ src/main/java/com/hendisantika/simplespringbootsecuritylogin/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.hendisantika.simplespringbootsecuritylogin.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-springboot-security-login
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-23
+ * Time: 05:07
+ */
+@Controller
+public class LoginController {
+    Logger log = LoggerFactory.getLogger(LoginController.class);
+
+    @RequestMapping("/login")
+    ModelAndView login(@RequestParam Optional<String> error) {
+        log.info("Calling login");
+        ModelAndView modelAndView = new ModelAndView("login/login");
+        if (error.isPresent()) {
+            modelAndView.addObject("error", "Invalid Credentials");
+        }
+        return modelAndView;
+    }
+}
