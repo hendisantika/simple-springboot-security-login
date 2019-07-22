42fb50082582f5227c3260bdf2c5efea46a7362f
hendisantika <hendisantika@yahoo.co.id>
2019-07-23 05:07:44 +0700

    #9 - Add HomeController

---
+++ src/main/java/com/hendisantika/simplespringbootsecuritylogin/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.hendisantika.simplespringbootsecuritylogin.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-springboot-security-login
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-23
+ * Time: 05:06
+ */
+@Controller
+public class HomeController {
+    Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping("/")
+    String index() {
+        log.info("Calling Index");
+        return "index";
+    }
+}
