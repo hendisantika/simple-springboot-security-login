69f29145a8d11b6f8a1e59831b5a50e07158dd18
hendisantika <hendisantika@yahoo.co.id>
2019-07-22 07:42:10 +0700

    #3 - Add Role Enum

---
+++ src/main/java/com/hendisantika/simplespringbootsecuritylogin/model/Role.java
@@ -0,0 +1,14 @@
+package com.hendisantika.simplespringbootsecuritylogin.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-springboot-security-login
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-22
+ * Time: 07:41
+ */
+public enum Role {
+    USER, ADMIN
+}
