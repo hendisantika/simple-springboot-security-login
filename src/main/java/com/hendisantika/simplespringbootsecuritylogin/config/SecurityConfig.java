c714fb4b08583e836d839c980ed9f40b43760607
Hendi Santika <hendisantika@gmail.com>
2020-03-20 09:55:06 +0700

    #26 - Bug Fix SecurityConfig & Login Page

--- src/main/java/com/hendisantika/simplespringbootsecuritylogin/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/simplespringbootsecuritylogin/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/assets/**").permitAll()
+                .antMatchers("/assets/**", "/bootstrap/**", "/jquery/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
